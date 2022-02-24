4f221a7650a9857f34594e865124f8f6fe26b0d2
Bornak <bornakpaul@gmail.com>
2022-02-24 23:33:17 +0530

    Package uuid added

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,9 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName : "Bornak",
-        lastName : "Paul",
-        age : 21,
-        designation : "Software Development Engineer"
-    },
-    {
-        firstName : "Bonney",
-        lastName : "Paul",
-        age : 28,
-        designation : "Freelancer/Fitness Instructor"
-    }
-];
+const users = [ ];
 
 //* all routes are stating with /users here.
 router.get('/', (req, res)=>{  
@@ -26,7 +14,8 @@ router.post('/', (req, res)=>{
     //console.log(req.body);
     //res.send("POST ROUTE REACHED");
     const user = req.body;
-    users.push(user);
+    
+    users.push({  ...user, id: uuidv4()});
     res.send(`User with the name ${user.firstName} added to the database.`);
 });
 
