e1aeb624fb749af34708d273de9c702e6ffbd896
Aimed Labs Dev Team | BPaul <contact.dev.team@aimedlabs.com>
2022-02-24 19:26:55 +0530

    post request

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import usersRoutes from './routes/users.js';
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => {
+    console.log('[TEST!]');
+
+    res.send('Hello from Homepage')
+});
+
+app.listen(PORT, ()=> console.log(`Server Running on port: http://localhost:${PORT}`));
